75876fe9f69f3ffd6ceea734953186e6716ada37
g12914iy <g12914iy@m20204.tsuda.ac.jp>
2014-04-29 18:49:25 +0900

    直してみました

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,12 +5,12 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
 	}
-	 
+
 	public static String camelToSnakecase(String camelcase) {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
@@ -19,16 +19,16 @@ public class SnakeCamelUtil {
 			if (Character.isUpperCase(c)) {
 				sb.append(camelcase.substring(j, i));
 				if (sb.length() > 0) {
-					sb.append("_");
+					sb.append("_");//ŠÔ‚É_‚ğ‹²‚Ş
 				}
-				sb.append(Character.toLowerCase(c));
-				j = i;
+				sb.append(Character.toLowerCase(c));//æ“ª‚ğ¬•¶š‚É
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
 		return new String(sb);
 	}
-	
+
 	static String capitalize(String s) {
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
