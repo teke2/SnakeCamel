f37754eb5b561a88812e27518baf2cb6f0058109
g12914iy <g12914iy@m20209.tsuda.ac.jp>
2014-05-06 15:14:28 +0900

    よけいな_が入っていても通るようにしました。

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -3,7 +3,7 @@ package snakecamel;
 public class SnakeCamelUtil {
 	
 	public static String snakeToCamelcase(String snake_case) {
-		String[] words = snake_case.split("_");
+		String[] words = snake_case.split("[_]+");
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
@@ -30,6 +30,7 @@ public class SnakeCamelUtil {
 	}
 
 	static String capitalize(String s) {
+		if(s.equals("")==true) return "";
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
 		String rest = s.substring(1);
@@ -37,6 +38,7 @@ public class SnakeCamelUtil {
 	}
 
 	static String uncapitalize(String s) {
+		if(s.equals("")==true) return "";
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -7,19 +7,63 @@ import org.junit.Test;
 
 public class SnakeCamelTest {
 	@Test
-	public void test2() {
+	public void snakeToCamelcase() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();		
-		String a ="test_case";
-		String expected = "TestCase";
-		String actual =scu.snakeToCamelcase(a);
-		assertThat(actual,is(expected));		
+		String expected = "Abc";
+		String expected1 = "AbcDef";
+		String expected2 = "AbcDefGh";
+		String expected3 = "AbcDefGh";
+		String expected4 = "AbcDef";
+		String actual = scu.snakeToCamelcase("abc");
+		String actual1 = scu.snakeToCamelcase("abc_def");
+		String actual2 = scu.snakeToCamelcase("abc_def_gh");
+		String actual3 = scu.snakeToCamelcase("abc__def___gh");
+		String actual4 = scu.snakeToCamelcase("_abc_def");
+		assertThat(actual,is(expected));
+		assertThat(actual1,is(expected1));
+		assertThat(actual2,is(expected2));	
+		assertThat(actual3,is(expected3));
+		assertThat(actual4,is(expected4));	
 	}
 	@Test
-	public void test3() {
+	public void camelToSnakecase() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();		
-		String a ="TestCase";
-		String expected = "test_case";
-		String actual =scu.camelToSnakecase(a);
+		String expected = "abc";
+		String expected1 = "abc_def";
+		String expected2 = "abc_def_gh";
+		String actual = scu.camelToSnakecase("Abc");
+		String actual1 = scu.camelToSnakecase("AbcDef");
+		String actual2 = scu.camelToSnakecase("AbcDefGh");
 		assertThat(actual,is(expected));
+		assertThat(actual1,is(expected1));
+		assertThat(actual2,is(expected2));	
+	}
+	@Test
+	public void capitalize() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();		
+		String expected = " ";
+		String expected1 = "A";
+		String expected2 = "Xyz";
+		String actual = scu.capitalize(" ");
+		String actual1 = scu.capitalize("a");
+		String actual2 = scu.capitalize("xyz");
+		assertThat(actual,is(expected));
+		assertThat(actual1,is(expected1));
+		assertThat(actual2,is(expected2));	
+
+	}
+	@Test
+	public void uncapitalize() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();		
+		String expected = " ";
+		String expected1 = "a";
+		String expected2 = "xyz";
+		String actual = scu.uncapitalize(" ");
+		String actual1 = scu.uncapitalize("A");
+		String actual2 = scu.uncapitalize("Xyz");
+		assertThat(actual,is(expected));
+		assertThat(actual1,is(expected1));
+		assertThat(actual2,is(expected2));	
+
 	}
 }
